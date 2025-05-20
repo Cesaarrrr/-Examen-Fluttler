b2298726b8a6ceaecbd98328000a0de1057e7929
Cesarrrr <cabreracesar644@gmail.com>
2025-05-20 12:44:17 -0500

    continuacion de la vista

---
+++ evidencia_flutter/lib/feature/home/presentation/view/carga_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Carga extends StatelessWidget {
+  const Carga({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
+
--- evidencia_flutter/lib/main.dart
+++ evidencia_flutter/lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'feature/home/presentation/view/carga_view.dart';
+import 'feature/home/presentation/view/failure_view.dart';
+import 'feature/home/presentation/view/inicial_view.dart';
+import 'feature/home/presentation/view/succes_view.dart';
+
 void main() {
   runApp(Myapp());
 }
@@ -8,31 +13,9 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            body: Stack(
-      fit: StackFit.expand,
-      children: [
-        Image.network(
-          'https://img.freepik.com/foto-gratis/deliciosa-hamburguesa-queso-papas-fritas_23-2148290584.jpg?semt=ais_hybrid&w=740',
-          fit: BoxFit.cover,
-        ),
-        Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Cesar´s Foods',
-                style: TextStyle(
-                  fontSize: 29,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.yellowAccent,
-                  shadows: [Shadow(blurRadius: 5, color: Colors.black)],
-                ),
-              )
-            ],
-          ),
-        )
-      ],
-    )));
+        home: Inicial());
   }
 }
+
+
+
